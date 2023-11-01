a3dd7d47695e07ac36ea8dbf0a2c9635b21dc96c
Bakermat <patrickpeeters@outlook.com>
2023-11-01 13:00:24 +0100

    Updating crypto library to 0.4

--- components/dsmr/__init__.py
+++ components/dsmr/__init__.py
@@ -82,7 +82,7 @@ async def to_code(config):
     cg.add_define("DSMR_GAS_MBUS_ID", config[CONF_GAS_MBUS_ID])
 
     # DSMR Parser
-    # cg.add_library("glmnet/Dsmr", "0.5")
+    # cg.add_library("glmnet/Dsmr", "0.8")
 
     # Crypto
-    cg.add_library("rweather/Crypto", "0.2.0")
+    cg.add_library("rweather/Crypto", "0.4.0")
